2088c8c437fe0e34341f03028a0ae5fe6aa3c783
iDrKm <josbytron935@gmail.com>
2025-04-15 10:21:23 -0400

    variables e input

--- hola.py
+++ hola.py
@@ -1,1 +1,20 @@
-print("hola mundo") 
+#variables
+#nombre="Jose"
+edad = 19
+
+#nombre=input()
+
+#ejemplo de concadenacion 
+#print("hola" + nombre , "y su edad es" , edad)
+
+#print("hola " + nombre , "y su edad en 5 años sera" , edad+5)
+
+
+
+n1=int(input("ingrese 1 numero "))
+
+n2=int(input("ingrese 1 numero "))
+
+print("el resultado de la suma es  " , n1+n2)
+
+
